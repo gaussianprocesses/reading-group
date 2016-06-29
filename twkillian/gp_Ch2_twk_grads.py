fd4641d2eeaa6176fd45139e6e62568d08a03ade
Taylor Killian <taylorkillian@g.harvard.edu>
2016-06-29 16:04:05 -0400

    Successfully implemented autograd to optimize length scales

--- twkillian/gp_Ch2_twk_grads.py
+++ twkillian/gp_Ch2_twk_grads.py
@@ -1,9 +1,11 @@
 '''The following script is my attempt at translating r code from @mymakar into python'''
 
 import autograd.numpy as np   # Thinly-wrapped version of Numpy
-from autograd import grad
+import autograd.numpy.random as npr
+from autograd import grad, value_and_grad
 import matplotlib.pyplot as plt
 from scipy.stats import multivariate_normal
+from scipy.optimize import minimize
 import random
 
 # Calculates the covariance matrix sigma
@@ -18,19 +20,10 @@ def calcSigma(x1, x2,l):
 		--------------------------------------------------------
 		Notes:
 	'''
-	Sigma = np.zeros((len(x1),len(x2)))
-	for ii in range(len(x1)):
-		for jj in range(len(x2)):
-			delta = x1[ii]-x2[jj]
-			numer = np.abs(delta)
-			exponent = -0.5*(numer/l)**2
-			temp = float(np.exp(exponent))
-			# temp = float(np.exp(-0.5*(np.abs(x1[ii]-x2[jj])/l)**2))
-			Sigma[ii, jj] = temp
-			print type(ii), type(jj), type(x1[ii]), type(x2[jj]), type(l)
-			print type(Sigma[ii,jj])
-
-	return Sigma
+	length_scale = l
+	diffs = np.expand_dims(x1 /length_scale,1)\
+		 - np.expand_dims(x2 /length_scale,0)
+	return np.exp(-0.5 * np.sum(diffs**2,axis=2))
 
 def sample_GP(num_functions,n_pts,mean,cov):
 	''' From updated mean and covariance, sample the num_functions from the prescribed GP
@@ -87,13 +80,16 @@ if __name__ == '__main__':
 	n_pts = 50
 	n_samples = 3
 	seed = 12345
+	rs = npr.RandomState(0)
+
+	num_params = 1
 
 	random.seed(seed)
 
 	# 1. Plot some sample functions from the GP
 
 	# Define the points at which we want to define the functions
-	x_star = np.linspace(x_domain[0],x_domain[1],num=n_pts)
+	x_star = np.reshape(np.linspace(x_domain[0],x_domain[1],num=n_pts),(n_pts,1))
 
 	# Calculate the covariance matrix between x_star and itself
 	sigma = calcSigma(x_star,x_star,length_scale)
@@ -102,16 +98,18 @@ if __name__ == '__main__':
 	sampled_values = sample_GP(n_samples,n_pts,np.zeros(n_pts),sigma)
 
 	# Plot the result
+	# import pdb
+	# pdb.set_trace()
 	plt.figure()
 	plt.plot(x_star,sampled_values[:,0],x_star,sampled_values[:,1],x_star,sampled_values[:,2])
-	plt.fill_between(x_star,-2.5*np.ones(n_pts),2.5*np.ones(n_pts),color='0.15',alpha=0.25)
+	plt.fill_between(x_star.flatten(),-2.5*np.ones(n_pts),2.5*np.ones(n_pts),color='0.15',alpha=0.25)
 	plt.xlabel('input, x')
 	plt.ylabel('output, f(x)')
 	plt.title('Sampling from Prior')
 	plt.show()
 
 	# 2. Now let's assume that we have some know data points;
-	x = np.array([-4, -3, -1, 0, 2])
+	x = np.reshape(np.array([-4, -3, -1, 0, 2]),(5,1))
 	y = np.array([-2, 0, 1, 2, -1])
 
 	# Calculate the covariance matrices
@@ -136,7 +134,7 @@ if __name__ == '__main__':
 	# Plot the results
 	plt.figure()
 	plt.plot(x_star,f_star_sampled_values[:,0],x_star,f_star_sampled_values[:,1],x_star,f_star_sampled_values[:,2])
-	plt.fill_between(x_star,func_lower,func_upper,color='0.15',alpha=0.25)
+	plt.fill_between(x_star.flatten(),func_lower,func_upper,color='0.15',alpha=0.25)
 	plt.xlabel('input, x')
 	plt.ylabel('output, f(x)')
 	plt.title("Prediction with noise-free observations")
@@ -162,7 +160,7 @@ if __name__ == '__main__':
 	# Plot the results
 	plt.figure()
 	plt.plot(x_star,f_bar_star_sampled_values[:,0],x_star,f_bar_star_sampled_values[:,1],x_star,f_bar_star_sampled_values[:,2])
-	plt.fill_between(x_star,func_bar_lower,func_bar_upper,color='0.15',alpha=0.25)
+	plt.fill_between(x_star.flatten(),func_bar_lower,func_bar_upper,color='0.15',alpha=0.25)
 	plt.xlabel('input, x')
 	plt.ylabel('output, f(x)')
 	plt.title("Prediction using noisy observations")
@@ -174,10 +172,10 @@ if __name__ == '__main__':
 
 	def marg_likelihood(x, y, l):
 		k_xx = calcSigma(x,x,l)
-		marg_data = 0.5* y.T.dot(np.linalg.inv(k_xx+ (sigma_n**2)*np.identity(k_xx.shape[0])).dot(y)) - 0.5 * \
+		marg_data = 0.5* np.dot(y.T,np.dot(np.linalg.inv(k_xx+ (sigma_n**2)*np.identity(k_xx.shape[0])),y)) - 0.5 * \
 			np.log(np.linalg.det(np.linalg.inv(k_xx+ (sigma_n**2)*np.identity(k_xx.shape[0])))) - (len(y)*0.5) * np.log(2*np.pi) 
 
-		return marg_data
+		return -1.0*marg_data
 
 
 	###################################
@@ -186,10 +184,47 @@ if __name__ == '__main__':
 
 	g_ml = lambda l: marg_likelihood(x,y,l)
 
+	init_params = 0.1 * rs.randn(num_params)
 	grad_ml = grad(g_ml)
+	cov_params = minimize(value_and_grad(g_ml),init_params,jac=True,
+						method = 'CG')
 
 	print marg_likelihood(x,y,length_scale)
 	print grad_ml(length_scale)
+	print "Initial Parameters: ", init_params
+	print "Optimized Parameters: ", cov_params.x
+
+	opt_length_scale = np.exp(cov_params.x[0])
+
+	# Calculate the covariance matrices with optimized length scale
+	# to "condition" based on the observed (top of page 16)
+	# this is the part that confused @mymakar, the following follows the text verbatim
+	ok_xx = calcSigma(x,x,opt_length_scale)
+	ok_xxs = calcSigma(x,x_star,opt_length_scale)
+	ok_xsx = calcSigma(x_star,x,opt_length_scale)
+	ok_xsxs = calcSigma(x_star,x_star,opt_length_scale)
+
+
+	# Update the mean and covariance from equations 2.22-2.24
+	of_bar_star_mean = ok_xsx.dot(np.linalg.inv(ok_xx+ (sigma_n**2)*np.identity(ok_xx.shape[0])).dot(y))
+	of_bar_star_cov = ok_xsxs - ok_xsx.dot(np.linalg.inv(ok_xx+ (sigma_n**2)*np.identity(ok_xx.shape[0])).dot(ok_xxs))
+
+	# Redraw the sample functions
+	of_bar_star_sampled_values = sample_GP(n_samples*10,n_pts,of_bar_star_mean,of_bar_star_cov)
+
+	# Get mean and spread of newly sampled values
+	ofunc_bar_mean, ofunc_bar_lower, ofunc_bar_upper = calculate_func_mean_and_variance(of_bar_star_sampled_values)
+
+	# Plot the results
+	plt.figure()
+	plt.plot(x_star,of_bar_star_sampled_values[:,0],x_star,of_bar_star_sampled_values[:,1],x_star,of_bar_star_sampled_values[:,2])
+	plt.fill_between(x_star.flatten(),ofunc_bar_lower,ofunc_bar_upper,color='0.15',alpha=0.25)
+	plt.xlabel('input, x')
+	plt.ylabel('output, f(x)')
+	plt.title("Prediction using noisy observations, with optimized length scale")
+	plt.show()
+
+
 
 
 
